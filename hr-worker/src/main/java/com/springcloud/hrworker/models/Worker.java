f665195288855e001fe468982843a1a18cf3cfe2
Gabriel <g.moraes.garcia@gmail.com>
2022-02-09 01:53:10 -0300

    hr-worker postgres dev profile

--- hr-worker/src/main/java/com/springcloud/hrworker/models/Worker.java
+++ hr-worker/src/main/java/com/springcloud/hrworker/models/Worker.java
@@ -7,12 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 
 @Getter
 @Setter
 @Entity
+@Table(name = "tb_worker")
 public class Worker implements Serializable {
 
     private static  final long serialVersionUID =1L;
@@ -30,4 +32,6 @@ public class Worker implements Serializable {
         this.name = name;
         this.dailyIncome = dailyIncome;
     }
+
+
 }
--- hr-worker/src/main/java/com/springcloud/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/springcloud/hrworker/resources/WorkerResource.java
@@ -39,11 +39,6 @@ public class WorkerResource {
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
 
-       /* try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
 
         logger.info("Port = " + environment.getProperty("local.server.port"));
 
